9318f393e7c499ca2f959243146891c8e41f7b08
Ahmed Fouad <ahmedfouad01099@gmail.com>
2021-06-02 00:40:13 +0200

    adding components

---
+++ components/CategoryGritTile.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TouchableNativeFeedback,
+} from "react-native";
+
+function CategoryGritTile(props) {
+  let TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.gridItem}>
+      <TouchableCmp style={{ flex: 1 }} onPress={props.onSelect}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text numberOfLines={2} style={styles.title}>
+            {props.title}
+          </Text>
+        </View>
+      </TouchableCmp>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "right",
+  },
+});
+
+export default CategoryGritTile;
